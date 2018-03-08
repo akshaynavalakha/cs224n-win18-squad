940d4f4a843918d7a0bb360120f5c31780bd9ffb
Subin Modeel <subin@redhat.com>
2018-03-07 22:38:52 -0500

    Update dcan.py

--- code/dcan.py
+++ code/dcan.py
@@ -29,7 +29,7 @@ class LSTMEncoder(object):
           hidden_size: int. Hidden size of the RNN
           keep_prob: Tensor containing a single scalar that is the keep probability (for dropout)
         """
-        self.hidden_size = hidden_size/2 #200 so making it 100
+        self.hidden_size = hidden_size #200
         self.keep_prob = keep_prob
         self.lstm = tf.nn.rnn_cell.LSTMCell(self.hidden_size)
         #self.question_length = 30
@@ -42,23 +42,34 @@ class LSTMEncoder(object):
             inputs_size=inputs.get_shape().as_list()
             inputs_temp=inputs
 
-            c_or_q, _ = tf.nn.dynamic_rnn(self.lstm, inputs_temp, dtype=tf.float32)
-            if type=="query":
-                query_encoding = tf.layers.dense(inputs_temp, inputs_temp.get_shape()[2], activation=tf.tanh)
-                inputs_temp=query_encoding
+            # 1) get encoding from LSTM
+            C_or_Q, _ = tf.nn.dynamic_rnn(self.lstm, inputs_temp, dtype=tf.float32)
+
+
+            if type=="question":
+                # IF it is Question_hidden
+                # Calculate q_dash = tanh(W q + b)
+                q_dash = tf.layers.dense(inputs_temp, C_or_Q.get_shape()[2], activation=tf.tanh)
+                inputs_temp = q_dash
                 sentinel = tf.get_variable("sentinel_q", [1, self.hidden_size], initializer=tf.random_normal_initializer())
             else:
-                sentinel = tf.get_variable("sentinel_c", [1, self.hidden_size], initializer=tf.random_normal_initializer())
-            sentinel = tf.reshape(sentinel, (1, 1, -1))
-            sentinel = tf.tile(sentinel, (tf.shape(inputs_temp)[0], 1, 1))
-            c_or_q_enhanced = tf.concat([sentinel, inputs_temp], 1)
+                # IF it is Context_hidden
+                sentinel = tf.get_variable("sentinel_c", [1, self.hidden_size], initializer=tf.random_normal_initializer()) # 1,200
+                inputs_temp = C_or_Q
+            #
+            # reshape sentinel
+            sentinel = tf.reshape(sentinel, (1, 1, -1)) #1, 1, 200
+            # reshape sentinel to add batch
+            sentinel = tf.tile(sentinel, (tf.shape(inputs_temp)[0], 1, 1)) #?, 1, 200
+            # add sentinel at end
+            out = tf.concat([inputs_temp, sentinel], 1) # ?, 601, 200
 
-            c_or_q_enhanced.get_shape().as_list()
+            out.get_shape().as_list()
 
             # Apply dropout
-            #out = tf.nn.dropout(out, self.keep_prob)
+            out = tf.nn.dropout(out, self.keep_prob)
 
-            return c_or_q_enhanced
+            return out
 
 
 class CoAttention(object):
@@ -71,22 +82,22 @@ class CoAttention(object):
           value_vec_size: size of the value vectors. int
         """
         self.keep_prob = keep_prob
-        self.context_hidden_size = context_hidden_size/2
-        self.query_hidden_size = query_hidden_size/2
+        self.context_hidden_size = context_hidden_size
+        self.query_hidden_size = query_hidden_size
 
 
     def build_graph(self, question_hiddens, context_mask, context_hiddens):
 
         with vs.variable_scope('Coattention') as scope:
-            question_hiddens.get_shape().as_list() #? , 31, 100
-            context_hiddens.get_shape().as_list()  #? ,601, 100
+            question_hiddens.get_shape().as_list() #? , 31, 200
+            context_hiddens.get_shape().as_list()  #? ,601, 200
 
             question_length = tf.shape(question_hiddens)[1]
             context_length = tf.shape(context_hiddens)[1]
             keys_dim = tf.shape(context_hiddens)[2]
 
 
-            Q_tranpose = tf.transpose(question_hiddens, perm=[0, 2, 1]) #?, 100, 31
+            Q_tranpose = tf.transpose(question_hiddens, perm=[0, 2, 1]) #?, 200, 31
             # L = D.T * Q = C.T * Q
             L = tf.matmul(context_hiddens, Q_tranpose)  #?, 601, 31
 
@@ -103,25 +114,26 @@ class CoAttention(object):
 
             A_Q = tf.map_fn(lambda x: tf.nn.softmax(x), L, dtype=tf.float32) #?, 601, 31
 
-            C_Q = tf.matmul(tf.transpose(context_hiddens, perm=[0, 2, 1]), A_Q)  #?, 100, 31
+            C_Q = tf.matmul(tf.transpose(context_hiddens, perm=[0, 2, 1]), A_Q)  #?, 200, 31
 
-            Q_concat_CQ = tf.concat([Q_tranpose, C_Q], axis=1) #?,  200, 31
+            Q_concat_CQ = tf.concat([Q_tranpose, C_Q], axis=1) #?,  400, 31
 
-            C_D = tf.matmul(Q_concat_CQ, A_D) #?, 200, 601
+            C_D = tf.matmul(Q_concat_CQ, A_D) #?, 400, 601
 
-            CO_ATT = tf.concat([context_hiddens, tf.transpose(C_D, perm=[0, 2, 1])], axis=2) #?, 601, 300
+            CO_ATT = tf.concat([context_hiddens, tf.transpose(C_D, perm=[0, 2, 1])], axis=2) #?, 601, 600
 
             with tf.variable_scope('Coatt_encoder'):
                 # LSTM for coattention encoding
                 cell_fw = tf.nn.rnn_cell.LSTMCell(self.query_hidden_size)
                 cell_bw = tf.nn.rnn_cell.LSTMCell(self.query_hidden_size)
                 input_lens = tf.reduce_sum(context_mask, reduction_indices=1)
-                #?, 601, 100
+                #?, 601, 400
                 (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, CO_ATT,
                                                        dtype=tf.float32, sequence_length=input_lens+1)
                 U_1 = tf.concat([fw_out, bw_out], axis=2)
-                #U_1=U
+                
                 dims = U_1.get_shape().as_list()
+                # Remove the sentinel vector from last row
                 U_2 = tf.slice(U_1, [0,0,0], [tf.shape(U_1)[0], dims[1]-1, dims[2]])
                 U_2 = tf.reshape(U_2, [tf.shape(U_1)[0], dims[1]-1, dims[2]])
                 U_3 = tf.nn.dropout(U_2, self.keep_prob)
